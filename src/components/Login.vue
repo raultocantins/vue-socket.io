e6930b87de8ca2c211c68abd8e46c8b2aa977f48
Alex Raul Santo Araujo <06562980143@caltins.local>
2020-08-28 14:58:08 -0300

    Project of the a chatbox with Socket.io using vue

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="box-login">
+<h1>
+    Box-Login
+</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {
+  
+  }
+}
+</script>
+
+<style>
+
+</style>
